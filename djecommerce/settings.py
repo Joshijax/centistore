6f8cc36cc9d2c02e195c635493b123dd8ccbb9a9
Joshijax <joshijax08@gmail.com>
2023-02-28 10:26:21 +0100

    commit Message

--- djecommerce/settings.py
+++ djecommerce/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'jm@grao=k5j(av@a%5f#ar&a*5juffs*cnwt91b(6g94gs*ky2'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 ALLOWED_HOSTS = ['127.0.0.1', "centiacollection.herokuapp.com", "centiastore.com", "www.centiastore.com"]
 
 
