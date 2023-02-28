759dcc8dde578947d47697b52492c073fbb91947
Joshijax <joshijax08@gmail.com>
2023-02-28 07:31:51 +0100

    commit Message

--- static_root/assets/js/main.js
+++ static_root/assets/js/main.js
@@ -23,7 +23,7 @@
             axilInit.menuLinkActive();
             axilInit.headerIconToggle();
             axilInit.priceRangeSlider();
-            axilInit.quantityRanger();
+            // axilInit.quantityRanger();
             axilInit.axilSlickActivation();
             axilInit.countdownInit('.coming-countdown', '2022/10/01');
             axilInit.campaignCountdown('.campaign-countdown', '2022/10/01');
@@ -214,25 +214,25 @@
 
         },
 
-        quantityRanger: function() {
-            $('.pro-qty').prepend('<span class="dec qtybtn">-</span>');
-            $('.pro-qty').append('<span class="inc qtybtn">+</span>');
-            $('.qtybtn').on('click', function() {
-                var $button = $(this);
-                var oldValue = $button.parent().find('input').val();
-                if ($button.hasClass('inc')) {
-                    var newVal = parseFloat(oldValue) + 1;
-                } else {
-                    // Don't allow decrementing below zero
-                    if (oldValue > 0) {
-                        var newVal = parseFloat(oldValue) - 1;
-                    } else {
-                        newVal = 0;
-                    }
-                }
-                $button.parent().find('input').val(newVal);
-            });
-        },
+        // quantityRanger: function() {
+        //     $('.pro-qty').prepend('<span class="dec qtybtn">-</span>');
+        //     $('.pro-qty').append('<span class="inc qtybtn">+</span>');
+        //     $('.qtybtn').on('click', function() {
+        //         var $button = $(this);
+        //         var oldValue = $button.parent().find('input').val();
+        //         if ($button.hasClass('inc')) {
+        //             var newVal = parseFloat(oldValue) + 1;
+        //         } else {
+        //             // Don't allow decrementing below zero
+        //             if (oldValue > 0) {
+        //                 var newVal = parseFloat(oldValue) - 1;
+        //             } else {
+        //                 newVal = 0;
+        //             }
+        //         }
+        //         $button.parent().find('input').val(newVal);
+        //     });
+        // },
 
         axilSlickActivation: function(e) {
             $('.categrie-product-activation').slick({
